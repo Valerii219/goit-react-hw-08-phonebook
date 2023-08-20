73c79018d158b6427a3115e29750dbf1e006d2bf
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-08-20 20:13:44 +0200

    save

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import LoginForm from 'components/LoginForm/LoginForm'
 import React from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { loginThunk } from 'store/auth/actions';
 import css from './login.module.css'
@@ -8,14 +8,20 @@ import { useNavigate } from 'react-router';
 import { Button } from '@mui/material';
 import {  toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { selectAuth } from 'store/auth/selectors';
 
 const Login = () => {
   const dispatch = useDispatch();
+  const isAuth = useSelector(selectAuth);
   const navigate = useNavigate();
   const login = async body => {
     try {
       await dispatch(loginThunk(body)).unwrap()
       toast.success("Login successful!");
+      if (isAuth) {
+        navigate('/contacts'); // Виправлено виклик navigate
+        console.log('Navigated to /contacts'); // Додано console.log
+      }
     } catch (error) {
       toast.error("Login or password is incorrect ");
     }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,17 +1,22 @@
 import SignUpForm from 'components/SignUpForm/SignUpForm';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 
 import { signUpThunk } from 'store/auth/actions';
+import { selectAuth } from 'store/auth/selectors';
 
 
 const SignUp = () => {
- 
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const isAuth = useSelector(selectAuth);
   const auth = async body => {
     try {
       await dispatch(signUpThunk(body)).unwrap()
       toast.success("Registration is successful!");
+      if (isAuth) {
+        navigate('./contacts')}
     } catch (error) {
       toast.error("Login or password is incorrect or the user with such email is already registered ");
     }
