9998a0c8db7aa8f0e80ab5de85b32add8586c6d7
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-08-20 18:59:33 +0200

    div

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,29 +3,27 @@ import { Route, Routes } from "react-router";
 import Layout from "Layout/Layout";
 
 
-// import Home from "Pages/Home";
+import Home from "Pages/Home";
 import PublicGuards from "Guard/PublicGuard";
 import SignUp from "Pages/SignUp";
 import Login from "Pages/Login";
 import { ToastContainer } from "react-toastify";
-// import ContactFormik from "Pages/ContactFormik";
+import ContactFormik from "Pages/ContactFormik";
 
 export const App = () => {
  
   return (
-    <><ToastContainer autoClose={2300}/>
+    <div><ToastContainer autoClose={2300}/>
     <Routes>
       <Route path="/" element={<Layout/>}>
-        {/* <Route index element={<Home/>}/> */}
-        {/* <Route path="contacts" element={<ContactFormik/>}/> */}
+        <Route index element={<Home/>}/>
+        <Route path="contacts" element={<ContactFormik/>}/>
         <Route path="register" element={<PublicGuards><SignUp/></PublicGuards>}/>
         <Route path="login" element={<PublicGuards><Login/> </PublicGuards>}/>
       </Route>
-      
-      
     </Routes>
 
-    </>
+    </div>
     
   );
 
