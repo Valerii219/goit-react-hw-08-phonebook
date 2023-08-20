fa3ea40cc6a84305e36f0ba5d90378780a0859b6
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-08-20 18:49:00 +0200

    we

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router";
 
 import Layout from "Layout/Layout";
 
-import ContactFormik from "Pages/ContactFormik";
+
 import Home from "Pages/Home";
 import PublicGuards from "Guard/PublicGuard";
 import SignUp from "Pages/SignUp";
 import Login from "Pages/Login";
 import { ToastContainer } from "react-toastify";
+// import ContactFormik from "Pages/ContactFormik";
 
 export const App = () => {
  
@@ -16,7 +17,7 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route index element={<Home/>}/>
-        <Route path="contacts" element={<ContactFormik/>}/>
+        {/* <Route path="contacts" element={<ContactFormik/>}/> */}
         <Route path="register" element={<PublicGuards><SignUp/></PublicGuards>}/>
         <Route path="login" element={<PublicGuards><Login/> </PublicGuards>}/>
       </Route>
