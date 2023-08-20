a3edb85960054b8802984c42178c8176f2271c19
Valerii Arlamovskyi <valera.arl96@gmail.com>
2023-08-20 18:52:02 +0200

    ff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router";
 import Layout from "Layout/Layout";
 
 
-import Home from "Pages/Home";
+// import Home from "Pages/Home";
 import PublicGuards from "Guard/PublicGuard";
 import SignUp from "Pages/SignUp";
 import Login from "Pages/Login";
@@ -16,7 +16,7 @@ export const App = () => {
     <><ToastContainer autoClose={2300}/>
     <Routes>
       <Route path="/" element={<Layout/>}>
-        <Route index element={<Home/>}/>
+        {/* <Route index element={<Home/>}/> */}
         {/* <Route path="contacts" element={<ContactFormik/>}/> */}
         <Route path="register" element={<PublicGuards><SignUp/></PublicGuards>}/>
         <Route path="login" element={<PublicGuards><Login/> </PublicGuards>}/>
